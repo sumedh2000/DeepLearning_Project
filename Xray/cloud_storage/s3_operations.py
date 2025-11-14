e25c30183d7dfc0e43fcc3c39f5070f1e64648ec
sumedh2000 <sumedhbhatwadekar122@gmail.com>
2025-11-14 17:45:15 +0530

    After writing Basic

---
+++ Xray/cloud_storage/s3_operations.py
@@ -0,0 +1,15 @@
+import os
+import sys
+from Xray.exception import XrayException
+
+class s3Operation:
+    def sync_folder_to_s3() -> None:
+        try:
+            pass
+        except Exception as e:
+            raise XrayException(e,sys)
+    def sync_folder_to_s3() -> None:
+        try:
+            pass
+        except Exception as e:
+            raise XrayException(e,sys)
---
+++ Xray/components/data_ingestion.py
@@ -0,0 +1,22 @@
+import os
+import sys
+from Xray.cloud_storage.s3_operations import s3Operation
+from Xray.entity.artifact_entity import DataIngestionArtifact
+from Xray.entity.config_entity import DataIngestionConfig
+from Xray.exception import XrayException
+from Xray.logger import logging
+
+class DataIngestion:
+    def __init__(self):
+        pass
+    def get_data_from_s3(self):
+        try:
+            pass
+        except Exception as e:
+            raise XrayException(e,sys)
+        
+    def initiate_data_ingestion(self):
+        try:
+            pass
+        except Exception as e:
+            raise XrayException(e,sys)
